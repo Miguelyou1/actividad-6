db52c935cfe52b194009f7d88b9534acc3e0767e
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-05-09 12:10:58 +0200

    script

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function sumar(a, b) {
     return a + b;
 }
+
 function restar(a, b) {
     return a - b;
 }
+
 function multiplicar(a, b) {
     return a * b;
 }
+
 function dividir(a, b) {
     if (b === 0) {
         console.log("No se puede dividir por cero ");
@@ -14,8 +17,10 @@ function dividir(a, b) {
     }
     return a / b;
 }
+
 var Numero1 = parseInt(prompt("Introduzca el primer número:"));
 var Numero2 = parseInt(prompt("Introduzca el segundo número:"));
+
 console.log("La suma es " + sumar(Numero1, Numero2));
 console.log("La resta es " + restar(Numero1, Numero2));
 console.log("La multiplicación es " + multiplicar(Numero1, Numero2));
