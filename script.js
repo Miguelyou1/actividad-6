d5273387c31ca853d73ced928e473f9ec8665f7e
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-05-10 19:39:55 +0200

    script

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
+// Función para sumar dos números
 function sumar(a, b) {
-    return a + b;
-}
-function restar(a, b) {
-    return a - b;
-}
-function multiplicar(a, b) {
-    return a * b;
-}
-function dividir(a, b) {
-    if (b === 0) {
-        console.log("No se puede dividir por cero ");
-        return;
-    }
-    return a / b;
-}
-var Numero1 = parseInt(prompt("Introduzca un número:"));
-var Numero2 = parseInt(prompt("Introduzca otro número:"));
-
-console.log("La suma es " + sumar(Numero1, Numero2));
-console.log("La resta es " + restar(Numero1, Numero2));
-console.log("La multiplicación es " + multiplicar(Numero1, Numero2));
-console.log("La división es " + dividir(Numero1, Numero2));
+    console.log(a + b);
+  }
