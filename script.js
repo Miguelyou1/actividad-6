6250ab7f01a326183629e68917737b1bd5506448
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-05-10 19:41:27 +0200

    script

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Función para sumar dos números
 function sumar(a, b) {
     console.log(a + b);
-  }
+}
 // Función para restar dos números
 function restar(a, b) {
     console.log(a - b);
-  }
+}
+// Función para multiplicar dos números
+function multiplicar(a, b) {
+    console.log(a * b);
+}
