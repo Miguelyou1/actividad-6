9107a5a615bb763d3e3516be6988bfeecaec2553
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-05-11 18:47:24 +0200

    script

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-// Función para sumar dos números
-function sumar(a, b) {
-    console.log(a + b);
+console.log("a continuación podrás escribir dos números para poderlos sumar, restar, multiplicar o dividir");
+console.log("escriba el primer número");
+let primernum = prompt();
+let num1 = parseInt(primernum);
+console.log("escriba el seguno número");
+let segundonum = prompt();
+let num2 = parseInt(segundonum);
+
+suma(num1, num2);
+function suma(suma1, suma2) {
+    let resultado = suma1 + suma2;
+    console.log("la suma de " + suma1 + " y " + suma2 + " es: " + resultado);
+    return resultado;
 }
-// Función para restar dos números
-function restar(a, b) {
-    console.log(a - b);
+
+resta(num1, num2);
+function resta(minuendo, sustraendo) {
+    let resultado = minuendo - sustraendo;
+    console.log("la resta de " + minuendo + " y " + sustraendo + " es: " + resultado);
+    return resultado;
 }
-// Función para multiplicar dos números
-function multiplicar(a, b) {
-    console.log(a * b);
+
+producto(num1, num2);
+function producto(Fact1, Fact2) {
+    let resultado = Fact1 * Fact2;
+    console.log("la multiplicación de " + Fact1 + " y " + Fact2 + " es: " + resultado);
+    return resultado;
 }
-// Función para dividir dos números
-function dividir(a, b) {
-    console.log(a / b);
-  }
+
+division(num1, num2);
+function division(Divi, div) {
+    let resultado = Divi / div;
+    console.log("la división de " + Divi + " y " + div + " es: " + resultado);
+    return resultado;
+}
